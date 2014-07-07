3828174dc00008f7fa0313e37ed83e425094cd2a
Chase Moody <chase.moody@mutualmobile.com>
2014-07-07 13:52:32 -0500

    Allow all header

--- routes/uploads.js
+++ routes/uploads.js
@@ -4,6 +4,12 @@ var util = require("util");
 var fs = require("fs");
 var multer = require('multer');
 
+router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
 router.use(multer({
     dest: './uploads/',
     rename: function (fieldname, filename) {
