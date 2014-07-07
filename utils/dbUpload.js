0099e69d0b3b5edffcb99858634a9b0176d67c5e
Chase Moody <chase.moody@mutualmobile.com>
2014-07-07 13:43:30 -0500

    Created db and processor libs

--- utils/dbUpload.js
+++ utils/dbUpload.js
@@ -1,7 +1,24 @@
 // https://github.com/Leveton/node-parse-api
 var Parse = require('node-parse-api').Parse;
 
+var db = {};
 var APP_ID = '';
 var MASTER_KEY = '';
 
 var app = new Parse(APP_ID, MASTER_KEY);
+
+// Get all for organization
+db.getAllProducts = function(data) {
+	app.findMany('Foo', '', function (err, response) {
+	  console.log(response);
+	});
+};
+
+// Insert products
+db.insertProduct = function() {
+	app.insert('Foo', { foo: 'bar' }, function (err, response) {
+	  console.log(response);
+	});
+};
+
+module.exports = db;
